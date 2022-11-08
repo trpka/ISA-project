f0d886aeabf8f8a83ab75dcba85630f27214fd96
trpka <64914510+trpka@users.noreply.github.com>
2022-11-08 12:05:33 +0100

    fixed class

--- ISAproject/src/main/java/com/example/ISAproject/model/DonationTerms.java
+++ ISAproject/src/main/java/com/example/ISAproject/model/DonationTerms.java
@@ -81,10 +81,12 @@ public class DonationTerms {
 	public void setRegisteredUser(RegisteredUser registeredUser) {
 		this.registeredUser = registeredUser;
 	}
-	public BloodCenter getProfileCenter() {
+
+	public BloodCenter getBloodCenter() {
 		return bloodCenter;
 	}
-	public void setProfileCenter(BloodCenter bloodCenter) {
+
+	public void setBloodCenter(BloodCenter bloodCenter) {
 		this.bloodCenter = bloodCenter;
 	}
 
